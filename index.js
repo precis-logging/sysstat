833f2b4f7fab88aa605e4efa65121f434b57bbb3
Jeremy Darling <jeremy.darling@gmail.com>
2015-09-01 11:28:54 -0500

    Fixup for proxied endpoints

--- index.js
+++ index.js
@@ -130,6 +130,7 @@ Plugin.prototype.init = function(options){
 Plugin.prototype.register = function(options){
   var register = options.register;
   register({
+    proxy: options.proxy,
     ui: registerUi.call(this),
     server: routes.call(this)
   });
