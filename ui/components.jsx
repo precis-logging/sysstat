690919718966884cae4e033357cc695a81a6dab3
Jeremy Darling <jeremy.darling@gmail.com>
2016-05-02 14:01:53 -0500

    Changes for latest format

--- ui/components.jsx
+++ ui/components.jsx
@@ -186,7 +186,81 @@ var SystemStatus = React.createClass({
     var collapsable = typeof(this.props.collapsable)==='undefined'?true:this.props.collapsable;
     var mini = !!this.props.mini;
     var hoverText = this.state.hoverText;
-    var status = this.state.systems.map((system)=>{
+/*
+OLD:
+{
+    "_id" : ObjectId("572757d3ebd99c2f7d680015"),
+    "name" : "classroom-ui",
+    "envConfig" : "stg",
+    "appVersion" : "2.78.10",
+    "source" : "server",
+    "hostname" : "stg-use1c-pr-09-ocv2-02x78x10-0009",
+    "pid" : NumberInt(32047),
+    "level" : NumberInt(30),
+    "mem" : {
+        "rss" : NumberInt(470519808),
+        "heapTotal" : NumberInt(206666240),
+        "heapUsed" : NumberInt(138147448)
+    },
+    "uptime" : NumberInt(124880),
+    "inboundDepth" : NumberInt(0),
+    "outboundDepth" : NumberInt(0),
+    "cpu" : NumberInt(0),
+    "msg" : "Server Status",
+    "time" : ISODate("2016-05-02T13:36:19.617+0000"),
+    "v" : NumberInt(0)
+}
+*/
+/*
+NEW:
+{
+    "_id" : ObjectId("57275791db3986951f7f377b"),
+    "level" : NumberInt(20),
+    "levelName" : "INFO",
+    "dateTime" : ISODate("2016-05-02T13:35:13.651+0000"),
+    "host" : "stg-use1b-pr-09-ocv2-03x00x07-0004",
+    "pid" : NumberInt(8085),
+    "version" : "3.0.33",
+    "appName" : "Classroom-UI",
+    "env" : "stg",
+    "data" : [
+        "Server Status",
+        {
+            "mem" : {
+                "rss" : NumberInt(165756928),
+                "heapTotal" : NumberInt(137221632),
+                "heapUsed" : NumberInt(136030640)
+            },
+            "uptime" : 226556.073,
+            "cpu" : NumberInt(0)
+        }
+    ]
+}
+*/
+    var systems = this.state.systems.map((system)=>{
+      if(system.data && system.data[0] === 'Server Status'){
+        return {
+          "_id" : system._id,
+          "name" : system.appName,
+          "envConfig" : system.env,
+          "appVersion" : system.version,
+          "source" : system.source || "server",
+          "hostname" : system.host,
+          "pid" : system.pid,
+          "level" : system.level,
+          "mem" : system.data[1].mem,
+          "uptime" : system.data[1].uptime,
+          "inboundDepth" : NaN,
+          "outboundDepth" : NaN,
+          "cpu" : system.data[1].cpu,
+          "msg" : "Server Status",
+          "time" : new Date(Date.parse(system.dateTime)),
+          "v" : 1
+        };
+      }
+      return system;
+    });
+    var status = systems.map((system)=>{
       if(mini){
         return <SystemStatusMini
                 system={system}
